5cdfd99b60147e404f4aa5cf7f3be5dd19a5df4b
YussaMoney <highshow74@gmail.com>
2025-08-02 11:50:28 +0100

    Modified Index.js: - Redeclared the 'getHumanChoice' function to ensure it returns a valid choice and align with the rock-paper-scissors icon. - Added 'round' to playRound function. - Redeclared game function which will be called when the user clicks the "shoot" button and removed loop of rounds

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 let humanScore = 0;
 let computerScore = 0;
 let tieScore = 0;
-
+let round = 0;
+let humanSelection = null;
+let computerSelection = null;
 // Assigned Value to Win, Lose & Tie Variable
 const win = "You Win this game!";
 const lose = "You Lose this game!";
 const tie = "The result is a tie!";
+
+const gameIconsContainer = document.querySelector(".game-iconsContainer");
+const iconItem = document.querySelectorAll(".icon-item");
+
 
 // Declared getComputerChoice Function
 function getComputerChoice() {
@@ -15,39 +21,57 @@ function getComputerChoice() {
   return computerChoice[random];
 }
 
+function getHumanChoice() {
+  iconItem.forEach(icon => {
+    icon.addEventListener('click', () => {
+      humanSelection = icon.id.replace('-icon', '');
+      console.log(humanSelection);
+      return humanSelection;
+    });
+  });
+}
+
 // Declared playRound Function
 function playRound(humanSelection, computerSelection) {
   if (humanSelection === computerSelection) {
+    round++;
     tieScore++;
     console.log(`${humanSelection} vs ${computerSelection}`);
     return "It's a tie";
+
   } else if (humanSelection === "rock") {
     console.log(`${humanSelection} vs ${computerSelection}`);
     if (computerSelection === "paper") {
+      round++;
       computerScore++;
       return "You Lose! Paper beats Rock";
     }
     if (computerSelection === "scissors") {
+      round++;
       humanScore++;
       return "You Won! Rock beats Scissors";
     }
   } else if (humanSelection === "paper") {
     console.log(`${humanSelection} vs ${computerSelection}`);
     if (computerSelection === "rock") {
+      round++;
       humanScore++;
       return "You Won! Paper beats Rock";
     }
     if (computerSelection === "scissors") {
+      round++;
       computerScore++;
       return "You Lose! Scissors beats Paper";
     }
   } else if (humanSelection === "scissors") {
     console.log(`${humanSelection} vs ${computerSelection}`);
     if (computerSelection === "paper") {
+      round++;
       humanScore++;
       return "You Won! Scissors beats Paper";
     }
     if (computerSelection === "rock") {
+      round++;
       computerScore++;
       return "You Lose! Rock beats Scissors";
     }
@@ -55,46 +79,21 @@ function playRound(humanSelection, computerSelection) {
     return "Error! You've to choose one";
   }
 }
-
-// Declared Loop
+// Declared game Function
+// This function will be called when the user clicks the "shoot" button
 function game() {
-  for (let i = 1; i <= 5; i++) {
-    console.log(`Round ${i}`);
 
-    function getHumanChoice() {
-      let humanChoice = prompt(
-        `Input your choice of "Rock, Paper or Scissors"`
-      ).toLowerCase();
-      if (
-        humanChoice === "rock" ||
-        humanChoice === "paper" ||
-        humanChoice === "scissors"
-      ) {
-        return humanChoice;
-      } else if (
-        humanChoice === undefined ||
-        humanChoice === null ||
-        humanChoice.trim() === ""
-      ) {
-        alert(
-          `You didn't make a choice! Please choose "Rock", "Paper", or "Scissors".`
-        );
-        return getHumanChoice(); // Recursively ask for a valid choice
-      } else {
-        alert(`Invalid choice! Please choose "Rock", "Paper", or "Scissors".`);
-        return getHumanChoice(); // Recursively ask for a valid choice
-      }
-    }
+  computerSelection = getComputerChoice();
+  console.log(playRound(humanSelection, computerSelection));
 
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice();
+  console.log(
+    `Round ${round}
+    Human Score (${humanScore}) : Computer Score (${computerScore})`
+  );
 
-    console.log(playRound(humanSelection, computerSelection));
-    console.log(
-      `Human Score (${humanScore}) : Computer Score (${computerScore})`
-    );
-  }
-
+  humanSelection = null; // Reset humanSelection for the next round
+  computerSelection = null; // Reset computerSelection for the next round
+  
   if (humanScore === computerScore) {
     alert(tie.toUpperCase());
   } else if (humanScore > computerScore) {
@@ -103,18 +102,24 @@ function game() {
     alert(lose.toUpperCase());
   }
   alert("GAME OVER");
-  return "";
+  // return;
 }
-// console.log(game());
+
+// Function to change the button attribute after clicking "start"
+// This function will change the button text to "shoot" and update its ID
 let shootButtonAttribute = () => {
   const shootButton = document.querySelector("#start");
   shootButton.innerText = "shoot";
   shootButton.setAttribute("id", "shoot");
+  // shootButton.setAttribute("disabled", "disabled");
+  // shootButton.removeAttribute("id");
+  humanSelection = getHumanChoice();
+  shootButton.setAttribute("onclick", "game()");
 };
 
+// Function to switch the attribute of the game icons container
+// This function will change the ID of the container to "game-iconsContainerAfterStart"
 function switchAttribute() {
-  const gameIconsContainer = document.querySelector(".game-iconsContainer");
-  console.log(gameIconsContainer);
   gameIconsContainer.setAttribute("id", "game-iconsContainerAfterStart");
   gameIconsContainer.removeAttribute("class");
   shootButtonAttribute();
